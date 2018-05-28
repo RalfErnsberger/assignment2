5f1ace6047190d272368ec7a42aafd33960420b3
RalfErnsberger <39192911+RalfErnsberger@users.noreply.github.com>
2018-05-28 17:55:24 +0200

    Add files via upload

---
+++ Übung 2 BA1M.py
@@ -0,0 +1,18 @@
+def numberToPattern(x, k):
+    if k == 1:
+        return numberToSymbol(x)
+    return numberToPattern(x // 4, k - 1) + numberToSymbol(x % 4)
+
+
+def numberToSymbol(x):
+    if x == 0:
+        return "A"
+    if x == 1:
+        return "C"
+    if x == 2:
+        return "G"
+    if x == 3:
+        return "T"
+
+
+print(numberToPattern(45, 4))
